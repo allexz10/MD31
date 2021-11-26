f9ead48c5319d9f656bc05af78213fb3729ffaf7
Aleksejs Bojarcenko <82575361+allexz10@users.noreply.github.com>
2021-11-26 21:00:54 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Houses from './pages/Houses';
+import Characters from './pages/Characters';
+
+import './App.scss';
+
+const App = () => (
+  <>
+    <Routes>
+      <Route path="/" element={<Houses />} />
+      <Route path="/houses" element={<Houses />} />
+      <Route path="/characters/" element={<Characters />} />
+      <Route path="/characters/:slug" element={<Characters />} />
+      <Route path="/404" element={<NotFound />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </>
+);
+
+export default App;
